4127b1e6bd94c92ed2435576dc087751a30e470b
Cherryleafroad <13651622+MolotovCherry@users.noreply.github.com>
2023-06-10 15:43:46 -0700

    properly save html file at end to replace html elements

--- src/pipelines.rs
+++ src/pipelines.rs
@@ -5,23 +5,25 @@ use std::{fs, path::PathBuf, sync::Mutex};
 
 use js::process_js;
 
-use crate::asset_manager::AssetManager;
+use crate::asset_manager::{AssetManager, AssetType};
 use nipper::Document;
 
 pub struct PipelineProcessor {
     debug: bool,
     document: Document,
     asset_manager: Mutex<AssetManager>,
+    html_file: PathBuf,
 }
 
 impl PipelineProcessor {
     pub fn new(html_file: PathBuf, debug: bool) -> Self {
-        let html = fs::read_to_string(html_file).expect("Failed to read html file");
+        let html = fs::read_to_string(&html_file).expect("Failed to read html file");
 
         Self {
             debug,
             document: Document::from(&html),
             asset_manager: Mutex::new(AssetManager::new()),
+            html_file,
         }
     }
 
@@ -36,6 +38,11 @@ impl PipelineProcessor {
 
     fn finalize(&self) {
         let mut asset_manager = self.asset_manager.lock().unwrap();
+
+        // add processed html file now
+        let asset = AssetType::Memory((*self.document.html()).as_bytes().into());
+        asset_manager.add(asset, &self.html_file);
+
         asset_manager.dump().expect("Failed to dump assets");
     }
 }
