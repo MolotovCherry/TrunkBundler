37d0fc77624c2900dd3a1812d8097ed1042ecf31
Cherryleafroad <13651622+MolotovCherry@users.noreply.github.com>
2023-06-11 07:02:43 -0700

    Re-minify html, since it got changed on save

--- src/pipelines.rs
+++ src/pipelines.rs
@@ -3,6 +3,7 @@ mod js;
 use std::{fs, path::PathBuf, sync::Mutex};
 
 use js::process_js;
+use minify_html::Cfg;
 
 use crate::asset_manager::{AssetManager, AssetType};
 use nipper::Document;
@@ -38,8 +39,20 @@ impl PipelineProcessor {
     fn finalize(&self) {
         let mut asset_manager = self.asset_manager.lock().unwrap();
 
+        let html = if !self.debug {
+            let mut cfg = Cfg::spec_compliant();
+            // just in case
+            cfg.minify_css = true;
+            cfg.minify_js = true;
+            cfg.keep_closing_tags = true;
+
+            minify_html::minify((*self.document.html()).as_bytes(), &cfg)
+        } else {
+            (*self.document.html()).into()
+        };
+
         // add processed html file now
-        let asset = AssetType::Memory((*self.document.html()).as_bytes().into());
+        let asset = AssetType::Memory(html);
         asset_manager.add(asset, &self.html_file);
 
         asset_manager.dump().expect("Failed to dump assets");
